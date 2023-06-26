9043d58ecbf1b8a0515bec2c7b2136f2b4aaec14
kkang3617 <rkdgus3617@naver.com>
2023-06-26 18:23:40 +0900

    DAY87 23.06.26. 목록화면 처리중

--- src/component/todo/TodoTemplate.js
+++ src/component/todo/TodoTemplate.js
@@ -2,12 +2,29 @@ import React, { useEffect, useState } from 'react'
 import TodoHeader from './TodoHeader';
 import TodoMain from './TodoMain';
 import TodoInput from './TodoInput';
+import { useNavigate } from 'react-router-dom';
+import { Spinner } from 'reactstrap';
 
 import './scss/TodoTemplate.scss';
 
 import { API_BASE_URL as BASE, TODO } from '../../config/host-config';
+import { getLoginUserInfo } from '../../util/login-utils';
 
 const TodoTemplate = () => {
+
+  //로딩 상태값 관리
+  const [loading, setLoading] = useState(true);
+
+  const redirection = useNavigate();
+
+  // 로그인 인증 토큰 얻어오기
+  const { token } = getLoginUserInfo();
+
+  // 요청 헤더 설정
+  const requestHeader = {
+    'content-type' : 'application/json',
+    'Authorization' : 'Bearer ' + token // 오류!! 한칸띄워야함!!!!!!!
+  };
 
   // 서버에 할일 목록(json)을 요청(fetch)해서 받아와야 함.
   const API_BASE_URL = BASE + TODO; //'http://localhost:8181/api/todos';
@@ -17,10 +34,10 @@ const TodoTemplate = () => {
   const [todos, setTodos] = useState([]);
 
   // id 값 시퀀스 생성함수 
-  const makeNewId= () => {
-    if(todos.length === 0)return 1;
-    return todos[todos.length - 1 ].id + 1;
-  }
+  //const makeNewId= () => {
+  //  if(todos.length === 0)return 1;
+  //  return todos[todos.length - 1 ].id + 1;
+  //}
 
 
   // todoInput에게 todoText를 받아오는 함수
@@ -48,7 +65,7 @@ const TodoTemplate = () => {
 
     fetch(API_BASE_URL, {
       method : 'POST',
-      headers : {'content-type' : 'application/json'},
+      headers : requestHeader,//{'content-type' : 'application/json'},
       body : JSON.stringify(newTodo)
     })
     .then(res => res.json())
@@ -71,6 +88,7 @@ const TodoTemplate = () => {
 
     fetch(`${API_BASE_URL}/${id}`, {
       method : 'DELETE',
+      headers : requestHeader
     })
     .then(res => res.json())
     .then(json => {
@@ -85,7 +103,7 @@ const TodoTemplate = () => {
 
     fetch(API_BASE_URL, {
       method: 'PUT',
-      headers: {'content-type' : 'application/json'}, //application/json 타입 지정
+      headers: requestHeader,//{'content-type' : 'application/json'}, //application/json 타입 지정
       body : JSON.stringify({ //바디에 json데이터 변환해서 보냄.
         'done' : !done,   // body 안의 데이터
         'id' : id
@@ -110,9 +128,8 @@ const TodoTemplate = () => {
   }
 
   //체크가 안 된 할 일의 개수 카운트 하기
-  const countRestTodo = () => {
-    return todos.filter(todo => !todo.done).length; 
-  }
+  const countRestTodo = () => todos.filter(todo => !todo.done).length; 
+  
   // 새로운 배열을 받음 !todo.done 의미 할일 체크 x
     
 
@@ -120,25 +137,62 @@ const TodoTemplate = () => {
   //랜더링
   useEffect(() => {
     
-    //페이지가 렌더링 됨과 동시에 할 일 목록을 요청해서 뿌려 주겠다.
-    fetch(API_BASE_URL)
-      .then(res => res.json())
+    //페이지가 렌더링 됨과 동시에 할 일 목록을 요청해서 뿌려 주겠습니다.
+    fetch(API_BASE_URL, {
+      method: 'GET',
+      headers: requestHeader
+    })
+    .then(res => {
+      if(res.status === 200) return res.json();
+      else if(res.status === 403) {
+        alert('로그인이 필요한 서비스 입니다.');
+        redirection('/login');
+        return;
+      } else {
+        alert('관리자에게 문의하세요!');
+      }
+      return;
+      })
       .then(json => {
-        console.log(json.todos);
+        // console.log(json.todos);
 
         //fetch를 통해 받아온 데이터를 상태 변수에 할당.
         setTodos(json.todos);
       });
+        //로딩 완료 처리
+        setLoading(false);
 
   }, []); //[]배열에 변화가 있을 때~
 
-  return (
-    <div className='TodoTemplate'>
-        <TodoHeader count={countRestTodo}/> {/* 자식 컴포넌트들*/}
-        <TodoMain todoList={todos} remove={removeTodo} check={checkTodo}/>
-        <TodoInput addTodo={addTodo}/>
-    </div>
-  );
-}
-
-export default TodoTemplate;
+    // 로딩이 끝난 후 보여줄 컴포넌트
+    const loadEndedPage = (
+      <div className='TodoTemplate'>
+        <TodoHeader count={countRestTodo} />
+          <TodoMain 
+            todoList={todos} 
+            remove={removeTodo} 
+            check={checkTodo} 
+          />
+        <TodoInput addTodo={addTodo} />
+      </div>
+    );
+  
+    // 로딩 중일 때 보여줄 컴포넌트
+    const loadingPage = (
+      <div className='loading'>
+        <Spinner color='danger'>
+          loading...
+        </Spinner>
+      </div>
+    );
+  
+  
+    return (
+        <>
+          { loading ? loadingPage : loadEndedPage }
+        </>
+    );
+  
+  }
+  
+  export default TodoTemplate;
