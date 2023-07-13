e81a2c3c4ffd90da4c27bfeb2eb5b5bf2be9acc3
kkang3617 <rkdgus3617@naver.com>
2023-07-13 10:21:24 +0900

    배포 마지막 커밋

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -5,10 +5,9 @@ const clientHostName = window.location.hostname;
 let backEndHostName; //백엔드 서버 호스트 이름
 
 if(clientHostName === 'localhost') { //개발 중
-    // backEndHostName = 'http://localhost:8181';
+    backEndHostName = 'http://localhost:8181';
+} else if(clientHostName === 'my-todoapp-3617.s3-website.ap-northeast-2.amazonaws.com') { //배포해서 서비스 중
     backEndHostName = 'http://43.202.93.102';
-} else if(clientHostName === 'spring.com') { //배포해서 서비스 중
-    backEndHostName = 'https://api.spring.com';
 }
 
 export const API_BASE_URL = backEndHostName;
